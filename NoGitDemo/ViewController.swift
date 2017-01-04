d946b48ac689cf3d9de3ed628d93ba13cccbbdfb
Apple <apple@ABCD.local>
2017-01-04 17:03:21 +0530

    Initial commit

---
+++ NoGitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoGitDemo
+//
+//  Created by Apple on 04/01/17.
+//  Copyright © 2017 daemonSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
